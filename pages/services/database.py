11a57e61e2e1650c36061a665667c1c5b3233e14
ericsonmatheus <ericson.matheus.2016@gmail.com>
2025-03-20 09:23:58 -0300

    database (feat): add environ variables

--- pages/services/database.py
+++ pages/services/database.py
@@ -1,33 +1,35 @@
+import os
 import pandas as pd
+
 from sqlalchemy import create_engine, text
 from vlgi_datasets.postgres_upsert_table import PostgresUpsertFactory
 
 class Advisory:
     def __init__(self, connection):
         self.engine = create_engine(connection)
-    
-    def get_customers(self):
-        query = text("""
-            SELECT * FROM customers
-        """)
+        self.CUSTOMERS_DB = os.environ.get("CUSTOMERS_DB")
+        self.COLLABORATORS_DB = os.environ.get("COLLABORATORS_DB")
+        self.CUSTOMERS_RULES_DB = os.environ.get("CUSTOMERS_RULES_DB")
+        self.CONSTRAINT_CUSTOMERS_RULES = os.environ.get("CONSTRAINT_CUSTOMERS_RULES")
+
+    def get_customers(self) -> pd.DataFrame:
+        query = text(f"SELECT * FROM {self.CUSTOMERS_DB}")
         with self.engine.connect() as connection:
             customers = pd.read_sql(query, connection)
         return customers
 
-    def get_collaborators(self):
-        query = text("""
-            SELECT * FROM collaborators
-        """)
+    def get_collaborators(self) -> pd.DataFrame:
+        query = text(f"SELECT * FROM {self.COLLABORATORS_DB}")
         with self.engine.connect() as connection:
             collaborators = pd.read_sql(query, connection)
         return collaborators
 
-    def save_customers_collaborators(self, data: pd.DataFrame):
+    def save_customers_collaborators(self, data: pd.DataFrame) -> None:
         upsert_method_factory = PostgresUpsertFactory()
-        upsert_factor = upsert_method_factory.build(constraint="unique_customer_collaborator", overwrite_columns=True)
+        upsert_factor = upsert_method_factory.build(constraint=self.CONSTRAINT_CUSTOMERS_RULES, overwrite_columns=True)
         with self.engine.connect() as connection:
             data.to_sql(
-                "customers_collaborators",
+                self.CUSTOMERS_RULES_DB,
                 con=connection,
                 if_exists="append",
                 index=False,
